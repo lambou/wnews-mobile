ab2567a65cfad09db4a0ed02c445b3a245d9543b
Arnold LAMBOU <lambouarnold@gmail.com>
2020-02-09 05:31:57 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class Greeting extends Component<{name: string}, {}> {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    );
+  }
+}
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Greeting name="Amina" />
+      <Greeting name="Georges stinney" />
+      <Greeting name="Obama" />
+    </View>
+  );
+};
+
+export default App;
